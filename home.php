60694a444dfa7e54a41c109e446e9ecf6d46e856
Blopeeey <basweidmann@hotmail.com>
2021-09-22 10:09:53 +0200

    Convert to php and bootstrap

---
+++ home.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>MiRotterdam</title>
+        <link rel="stylesheet" href="css/global.css">
+        <script src="js/nav.js"></script>
+    </head>
+    <body>
+        <?php include "nav.php" ?>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="stylesheet" href="css/home.css">
+        <link rel="stylesheet" href="css/locations.css">
+        
+        <?php include "jq.php" ?>
+    </body>
+</html>
--- locations.php
+++ locations.php
@@ -1,10 +1,6 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>MiRotterdam</title>
-        <link rel="stylesheet" href="css/global.css">
-        <script src="js/nav.js"></script>
-    </head>
+    <?php include "nav.php" ?>
     <body>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         <link rel="stylesheet" href="css/locations.css">
