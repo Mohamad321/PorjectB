fc00109acefc54472217616f397c251f4d3e7842
Blopeeey <basweidmann@hotmail.com>
2021-09-22 11:16:57 +0200

    Converted prototype to php/bootstrap

--- home.php
+++ home.php
@@ -2,15 +2,117 @@
 <html>
     <head>
         <title>MiRotterdam</title>
-        <link rel="stylesheet" href="css/global.css">
+        <link rel="stylesheet" href="css/home.css">
         <script src="js/nav.js"></script>
     </head>
     <body>
         <?php include "nav.php" ?>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-        <link rel="stylesheet" href="css/home.css">
-        <link rel="stylesheet" href="css/locations.css">
-        
+        <!-- This div is  intentionally blank. It creates the transparent black overlay over the video which you can modify in the CSS -->
+        <header>
+        <div class="overlay"></div>
+
+        <!-- The HTML5 video element that will create the background video on the header -->
+        <video playsinline="playsinline" autoplay="autoplay" muted="muted" loop="loop">
+        <source src="content/homeVideo.mp4" type="video/mp4">
+        </video>
+
+        <!-- The header content -->
+        <div class="container h-100">
+        <div class="d-flex h-100 text-center align-items-center">
+            <div class="w-100" style="color: rgb(92, 139, 87);">
+            <h1 class="display-3">MiRotterdam</h1>
+            </div>
+        </div>
+        </div>
+        </header>
+
+        <section class="my-5">
+        <div class="container">
+
+            <!-- Page Heading -->
+            <h1 class="my-4">Locaties
+            </h1>
+
+            <!-- Project One -->
+            <div class="row">
+            <div class="col-md-7">
+                <a href="#">
+                <img class="img-fluid rounded mb-3 mb-md-0" src="https://via.placeholder.com/700x300" alt="">
+                </a>
+            </div>
+            <div class="col-md-5">
+                <h3>Project One</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium veniam exercitationem expedita laborum at voluptate. Labore, voluptates totam at aut nemo deserunt rem magni pariatur quos perspiciatis atque eveniet unde.</p>
+                <a class="btn btn-primary" href="#">View Project</a>
+            </div>
+            </div>
+            <!-- /.row -->
+
+            <hr>
+
+            <!-- Project Two -->
+            <div class="row">
+            <div class="col-md-7">
+                <a href="#">
+                <img class="img-fluid rounded mb-3 mb-md-0" src="https://via.placeholder.com/700x300" alt="">
+                </a>
+            </div>
+            <div class="col-md-5">
+                <h3>Project Two</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, odit velit cumque vero doloremque repellendus distinctio maiores rem expedita a nam vitae modi quidem similique ducimus! Velit, esse totam tempore.</p>
+                <a class="btn btn-primary" href="#">View Project</a>
+            </div>
+            </div>
+            <!-- /.row -->
+
+            <hr>
+
+            <!-- Project Three -->
+            <div class="row">
+            <div class="col-md-7">
+                <a href="#">
+                <img class="img-fluid rounded mb-3 mb-md-0" src="https://via.placeholder.com/700x300" alt="">
+                </a>
+            </div>
+            <div class="col-md-5">
+                <h3>Project Three</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis, temporibus, dolores, at, praesentium ut unde repudiandae voluptatum sit ab debitis suscipit fugiat natus velit excepturi amet commodi deleniti alias possimus!</p>
+                <a class="btn btn-primary" href="#">View Project</a>
+            </div>
+            </div>
+            <!-- /.row -->
+
+            <hr>
+
+            <!-- Pagination -->
+            <ul class="pagination justify-content-center">
+                <a href="locations.php" style="color: rgb(92, 139, 87);">Bekijk alle locaties</a>
+            </ul>
+
+        </div>
+        <!-- /.container -->
+        </section>
+
+
+        <section class="my-5" id="overons">
+        <div class="container">
+        <div class="container-fluid">
+        <div class="row">
+            <div class="col-sm-8">
+            <h2>Over ons</h2>
+            <h4>Lorem ipsum..</h4>
+            <p>Lorem ipsum..</p>
+            <button class="btn btn-default btn-lg">Get in Touch</button>
+            </div>
+            <div class="col-sm-4">
+            <span class="glyphicon glyphicon-signal logo"></span>
+            </div>
+        </div>
+        </div>
+        </div>
+        <!-- /.container -->
+        </section>
+          
         <?php include "jq.php" ?>
     </body>
 </html>
