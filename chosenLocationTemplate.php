d986d5004f1c7bd754b0db20fd7e7efcb47c3f4a
Blopeeey <basweidmann@hotmail.com>
2021-09-23 13:27:42 +0200

    location page

---
+++ chosenLocationTemplate.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>MiRotterdam</title>
+        <link rel="stylesheet" href="css/global.css">
+        <link rel="stylesheet" href="css/chosenLocation.css">
+        <script src="js/nav.js"></script>
+    </head>
+    <body>
+        <?php include "nav.php" ?>
+            <div class="container border-0 shadow rounded-3 overflow-hidden" style="padding-bottom: 20px; margin-top: 5%">
+
+            <!-- Portfolio Item Heading -->
+            <h1 class="my-3">Locatie 1
+            </h1>
+
+            <!-- Portfolio Item Row -->
+            <div class="row">
+
+            <div class="col-md-8">
+                <img class="img-fluid" src="https://images.unsplash.com/photo-1477959858617-67f85cf4f1df?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Y2l0eXxlbnwwfHwwfHw%3D&w=1000&q=80" alt="">
+            </div>
+
+            <div class="col-md-4">
+                <h3 class="my-3">Over deze locatie</h3>
+                <p>Locatie beschrijving</p>
+                <h3 class="my-3">Kies een thema</h3>
+                <div class="row">
+                    <div class="col">
+                        <img class="img-fluid" src="https://icons-for-free.com/iconfiles/png/512/sport+icon-1320195208765240116.png"><p class="carousel-caption text-primary" style="color: rgb(92, 139, 87); font-size: 20px;">Sport</p>
+                    </div>
+                    <div class="col">
+                        <img class="img-fluid" src="https://thefarmersdairy.co.za/wp-content/uploads/2017/06/green-icon-cropped-700x700-padding-1.jpg"><p class="carousel-caption" style="color: black; font-size: 20px;">Groen</p>
+                    </div>
+                    <div class="col">
+                        <img class="img-fluid" src="https://www.iconpacks.net/icons/2/free-safety-icon-3599-thumb.png">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col">
+                        Thema 4
+                    </div>
+                    <div class="col">
+                        Thema 5
+                    </div>
+                    <div class="col">
+                        Thema 6
+                    </div>
+                </div>
+            </div>
+
+            </div>
+            <!-- /.row -->
+
+            </div>
+            <!-- /.container -->
+        <?php include "jq.php" ?>
+    </body>
+</html>
--- home.php
+++ home.php
@@ -20,7 +20,7 @@
         <div class="container h-100">
         <div class="d-flex h-100 text-center align-items-center">
             <div class="w-100" style="color: rgb(92, 139, 87);">
-            <h1 class="display-3">MiRotterdam</h1>
+            <!-- <h1 class="display-3">MiRotterdam</h1> -->
             </div>
         </div>
         </div>
@@ -36,14 +36,14 @@
             <!-- Project One -->
             <div class="row">
             <div class="col-md-7">
-                <a href="#">
-                <img class="img-fluid rounded mb-3 mb-md-0" src="https://via.placeholder.com/700x300" alt="">
+                <a href="chosenLocationTemplate.php">
+                <img class="img-fluid rounded mb-3 mb-md-0" src="https://images.unsplash.com/photo-1477959858617-67f85cf4f1df?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Y2l0eXxlbnwwfHwwfHw%3D&w=1000&q=80" alt="">
                 </a>
             </div>
             <div class="col-md-5">
-                <h3>Project One</h3>
+                <h3>Locatie 1</h3>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium veniam exercitationem expedita laborum at voluptate. Labore, voluptates totam at aut nemo deserunt rem magni pariatur quos perspiciatis atque eveniet unde.</p>
-                <a class="btn btn-primary" href="#">View Project</a>
+                <a class="btn btn-primary" href="chosenLocationTemplate.php">Bekijk locatie</a>
             </div>
             </div>
             <!-- /.row -->
@@ -54,13 +54,13 @@
             <div class="row">
             <div class="col-md-7">
                 <a href="#">
-                <img class="img-fluid rounded mb-3 mb-md-0" src="https://via.placeholder.com/700x300" alt="">
+                <img class="img-fluid rounded mb-3 mb-md-0" src="https://images.unsplash.com/photo-1480714378408-67cf0d13bc1b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8&w=1000&q=80" alt="">
                 </a>
             </div>
             <div class="col-md-5">
-                <h3>Project Two</h3>
+                <h3>Locatie 2</h3>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, odit velit cumque vero doloremque repellendus distinctio maiores rem expedita a nam vitae modi quidem similique ducimus! Velit, esse totam tempore.</p>
-                <a class="btn btn-primary" href="#">View Project</a>
+                <a class="btn btn-primary" href="locations.php">Bekijk locatie</a>
             </div>
             </div>
             <!-- /.row -->
@@ -71,13 +71,13 @@
             <div class="row">
             <div class="col-md-7">
                 <a href="#">
-                <img class="img-fluid rounded mb-3 mb-md-0" src="https://via.placeholder.com/700x300" alt="">
+                <img class="img-fluid rounded mb-3 mb-md-0" src="https://cdn.vox-cdn.com/thumbor/Ksi3v8e1tj1ZcQeRV1ZtmCYIM7k=/0x0:4284x2916/1200x800/filters:focal(1800x1116:2484x1800)/cdn.vox-cdn.com/uploads/chorus_image/image/61918529/NorthBroadSt_Landscape_1_M.Edlow.0.jpg" alt="">
                 </a>
             </div>
             <div class="col-md-5">
-                <h3>Project Three</h3>
+                <h3>Locatie 3</h3>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis, temporibus, dolores, at, praesentium ut unde repudiandae voluptatum sit ab debitis suscipit fugiat natus velit excepturi amet commodi deleniti alias possimus!</p>
-                <a class="btn btn-primary" href="#">View Project</a>
+                <a class="btn btn-primary" href="locations.php">Bekijk locatie</a>
             </div>
             </div>
             <!-- /.row -->
@@ -94,7 +94,7 @@
         </section>
 
 
-        <section class="my-5" id="overons">
+        <section class="my-5" id="overons" style="background-color: rgb(247, 247, 247); padding-top: 20px; padding-bottom: 20px;">
         <div class="container">
         <div class="container-fluid">
         <div class="row">
@@ -113,6 +113,24 @@
         <!-- /.container -->
         </section>
           
+        <section class="my-5" id="contact">
+        <div class="container">
+        <div class="container-fluid">
+        <div class="row">
+            <div class="col-sm-8">
+            <h2>Contact</h2>
+            <h4>Lorem ipsum..</h4>
+            <p>Lorem ipsum..</p>
+            <button class="btn btn-default btn-lg">Get in Touch</button>
+            </div>
+            <div class="col-sm-4">
+            <span class="glyphicon glyphicon-signal logo"></span>
+            </div>
+        </div>
+        </div>
+        </div>
+        <!-- /.container -->
+        </section>
         <?php include "jq.php" ?>
     </body>
 </html>
